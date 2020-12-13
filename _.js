bbc972ed237aa5b7e35291b7af8c504852897803
Steve Larsen <slarsen-3@att.net>
2020-12-12 19:05:22 -0600

    added invert & findKey to lodash

--- _.js
+++ _.js
@@ -27,9 +27,37 @@ const _ = {
     }
     const startPaddingLength = Math.floor((length - string.length) / 2);
     const endPaddingLength = length - string.length - startPaddingLength;
-    const paddedString = ' '.repeat(startPaddingLength) + string + ' '.repeat(endPaddingLength);
+    const paddedString =
+      " ".repeat(startPaddingLength) + string + " ".repeat(endPaddingLength);
     return paddedString;
   },
+  has(object, key) {
+    const hasValue = object[key];
+    if (hasValue != undefined) {
+      return true;
+    }
+    return false;
+  },
+  invert(object) {
+    let invertedObject = {};
+    for (let key in object) {
+      const originalValue = object[key];
+      invertedObject = { originalValue: key };
+    }
+    return invertedObject;
+  },
+  findKey(object, predicate) {
+    for (let key in object) {
+      let value = object[key];
+      let predicateReturnValue = predicate(value);
+      if (predicateReturnValue) {
+        return key;
+      }
+    }
+    undefined;
+    return undefined;
+  },
+  
 };
 
 // Do not write or modify code below this line.
